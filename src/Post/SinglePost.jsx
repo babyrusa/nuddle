094f9876ef9c141bf183f4202a5ba2cd4222f3e7
Babyrusa <imdatingfood@gmail.com>
2020-02-08 17:17:09 -0500

    netlify -.-

--- src/Post/SinglePost.jsx
+++ src/Post/SinglePost.jsx
@@ -1,70 +1,76 @@
-import React, { Component } from 'react';
-import Post from '../models/Post';
-import PersonTop from '../Person/PersonTop';
+import React, { Component } from "react";
+import Post from "../models/Post";
+import PersonTop from "../Person/PersonTop";
+import { XCircle } from "react-feather";
 
 export default class SinglePost extends Component {
   constructor(props) {
-  	super(props);
+    super(props);
 
-  	this.state = {
-      post : {
-        attrs : {
-          username : 'Nudist',
-          base64 : null,
-          address : null
+    this.state = {
+      post: {
+        attrs: {
+          username: "Nudist",
+          base64: null,
+          address: null
         }
       },
-      img : ''
-  	};
+      img: ""
+    };
   }
-  componentDidMount(){
+  componentDidMount() {
     const id = this.props.match.params.postId;
     if (id) {
       this.getPost();
     } else {
-      this.props.history.push('/');
+      this.props.history.push("/");
     }
   }
-  async getPost(){
+  async getPost() {
     const id = this.props.match.params.postId;
-    const post = await Post.findOne({_id : id},{decrypt : false});
-    if(post) {
-      this.setState({
-        post : post
-      }, () => {
-        if (this.state.post.attrs.base64) {
-          this.setState({
-            img : this.state.post.attrs.base64
-          })
-        } else {
-          this.getImgFromGaia();
+    const post = await Post.findOne({ _id: id }, { decrypt: false });
+    if (post) {
+      this.setState(
+        {
+          post: post
+        },
+        () => {
+          if (this.state.post.attrs.base64) {
+            this.setState({
+              img: this.state.post.attrs.base64
+            });
+          } else {
+            this.getImgFromGaia();
+          }
         }
-      })
+      );
     } else {
-      this.props.history.push('/');
+      this.props.history.push("/");
     }
   }
-  async getImgFromGaia(){
-    const {post} = this.state;
-    const {userSession} = this.props;
-    const img = await userSession.getFile(`${post.attrs.address}.json`,
-    {
+  async getImgFromGaia() {
+    const { post } = this.state;
+    const { userSession } = this.props;
+    const img = await userSession.getFile(`${post.attrs.address}.json`, {
       username: post.attrs.username,
       decrypt: false
-    })
+    });
     await this.setState({
-      img : JSON.parse(img) || ''
-    })
+      img: JSON.parse(img) || ""
+    });
   }
   render() {
-    const {post, img} = this.state;
+    const { post, img } = this.state;
     return (
       <div className="single-post-wrapper">
         <div>
-        <img src={img}/>
-        <p>{post.attrs.caption}</p>
+          <button className="btn btn-outline-light delete-button">
+          <XCircle />
+          </button>
+          <img src={img} />
+          <p>{post.attrs.caption}</p>
         </div>
       </div>
-    )
+    );
   }
 }
